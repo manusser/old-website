6d35a45a1e398e48cf6fb78451f74f625287b8fa
cbrn <enfermados@protonmail.com>
2020-04-14 01:50:48 +0200

    added things

--- src/components/Proyectos.js
+++ src/components/Proyectos.js
@@ -21,10 +21,27 @@ class Proyectos extends Component{
           </Card.Title>
           <Card.Subtitle className="mb-2 text-muted">Forum</Card.Subtitle>
           <Card.Text>
-            Spanish forum founded at the beginning of 2018.
+            Spanish forum founded in 2018.
           </Card.Text>
           <Card.Link target="_blank" href="https://www.nodo313.net">Access</Card.Link>
           <Card.Link target="_blank" href="https://www.nodo313.net/index.php?members/cbrn.2/">My profile</Card.Link>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '18rem' }} className="col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe d-block mx-auto mx-md-0">
+        <Card.Body>
+          <Card.Title>Emojipedia <img
+                alt=""
+                src="https://cdn.shopify.com/s/files/1/1061/1924/files/Hugging_Face_Emoji_2028ce8b-c213-4d45-94aa-21e1a0842b4d_large.png?15202324258887420558"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />
+          </Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Service</Card.Subtitle>
+          <Card.Text>
+            Service to provide access to all the Emojis who exists
+          </Card.Text>
+          <Card.Link target="_blank" href="/emojis">Access</Card.Link>
         </Card.Body>
       </Card>
 
